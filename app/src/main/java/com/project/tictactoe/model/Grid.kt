868034a3e1009bc26e2bebcd9d793ce87a828ef0
2020-DEV-052 <dev202052 [at] gmail.com>
2020-03-07 17:50:49 +0100

    Initial commit

---
+++ app/src/main/java/com/project/tictactoe/model/Grid.kt
@@ -0,0 +1,3 @@
+package com.project.tictactoe.model
+
+class Grid
---
+++ app/src/main/java/com/project/tictactoe/viewmodel/GameGridViewModel.kt
@@ -0,0 +1,7 @@
+package com.project.tictactoe.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class GameGridViewModel : ViewModel() {
+
+}
