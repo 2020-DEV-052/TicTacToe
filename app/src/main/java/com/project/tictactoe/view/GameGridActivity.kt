e10e0d8366ffab075ff0457b4b0d38fc6e17a1da
2020-DEV-052 <dev202052 [at] gmail.com>
2020-03-08 00:58:30 +0100

    Implemented logic in activity to connect UI with GameGridViewModel

--- app/src/main/java/com/project/tictactoe/view/GameGridActivity.kt
+++ app/src/main/java/com/project/tictactoe/view/GameGridActivity.kt
@@ -2,13 +2,32 @@ package com.project.tictactoe.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.project.tictactoe.R
+import com.project.tictactoe.databinding.ActivityGridBinding
+import com.project.tictactoe.model.Player
+import com.project.tictactoe.viewmodel.GameGridViewModel
 
 class GameGridActivity : AppCompatActivity() {
 
+    private lateinit var viewModel: GameGridViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_grid)
+        startGame()
+    }
+
+    private fun startGame() {
+        val activityGridBinding: ActivityGridBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_grid)
+
+        viewModel = ViewModelProvider(this).get(GameGridViewModel::class.java)
+        viewModel.initializeGame()
+        activityGridBinding.viewModel = viewModel
+        viewModel.getWinner().observe(this, Observer<Player> {  })
+
     }
 
 }
